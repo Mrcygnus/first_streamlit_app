8d454987048556a07a9dc725d7db90386543edc4
Mrcygnus <aravind.p1099@gmail.com>
2022-11-30 16:07:30 +0530

    Text added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and  blueberry Oatmeal')
+streamlit.text('Kale,Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
 
 
 
