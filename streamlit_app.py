da285ed4714e8784086045d437e27e548fd3faf4
Mrcygnus <aravind.p1099@gmail.com>
2022-12-03 08:23:45 +0530

    frutyivce api called

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,34 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast    Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥗 Kale, Spinach &  Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avcado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas as pd
+my_fruit_list= pd.read_csv(r"https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+# Let's put a pick list here so they can pick the fruit they want to include 
+# Every multi select default values you give must be in list
+fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+#New section to display fruityivice api response 
+streamlit.header('Fruityvice Fruit Advice!')
+import requests
+fruityvice_response = requests.get(r"https://www.fruityvice.com/api/fruit/watermelon")
+# parses the json,returns dataframe with keys as columns and values as records
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+# output it to the screen
+streamlit.dataframe(fruityvice_normalized)
+
+
+
 
 
 
